c431901f9ce44e334586bbd4ff58fbcefaeddf71
RJ Aylward <rjaylwar@gmail.com>
2019-04-06 10:36:40 -0700

    removed welcome

---
+++ app/src/main/java/com/android1ucsd/week1/screen/welcome/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.android1ucsd.week1.screen.welcome;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by rjaylward on 4/6/19
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
